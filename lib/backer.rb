bbbb8b598677e3574bfb627bc56222869ef5a2c6
Matt Mason <masonmj10@gmail.com>
2020-08-10 04:00:04 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer 
   
   
-  attr_reader :backed_projects
+  attr_reader :backed_projects, :name 
   
-  def initialize
+  def initialize(name)
+    @name = name
     @backed_projects = []
   end 
   
